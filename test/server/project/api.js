b395905f9addd1fb50f935b5ddaa44712cb5afff
Erik Deijl <erik.deijl@gmail.com>
2014-05-12 15:45:04 +0200

    projects controller

--- test/server/project/api.js
+++ test/server/project/api.js
@@ -18,3 +18,18 @@ describe('GET /api/projects', function() {
       });
   });
 });
+
+describe('PUT /api/projects/:id', function() {
+  var project = {
+    name: 'Sample project 2'
+  };
+  it('should not update the object in the database if the user is not authenticated', function(done) {
+    request(app)
+      .put('/api/projects/5368e207bc36fb1a092a1c57', project)
+      .expect(401)
+      .end(function(err, res) {
+        if(err) return done(err);
+        done();
+      });
+  });
+});
